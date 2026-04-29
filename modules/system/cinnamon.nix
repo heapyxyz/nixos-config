dd0d32c2ece8192ba7ed4155b41de703d45e8fbe
heapy <63428380+heapyxyz@users.noreply.github.com>
2026-04-29 21:15:28 +0200

    feat(cinnamon): add dumped dconf settings

--- modules/system/cinnamon.nix
+++ modules/system/cinnamon.nix
@@ -1,4 +1,6 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
+
+with lib.gvariant;
 
 {
   # Enable the X11 windowing system.
@@ -40,4 +42,176 @@
 
   # disable cinnamon default apps
   services.cinnamon.apps.enable = false;
+
+  # dconf generated using dconf2nix
+  # https://github.com/gvolpe/dconf2nix
+  programs.dconf = {
+    enable = true;
+    profiles.user.databases = [
+      {
+        settings = {
+          "org/cinnamon" = {
+            enabled-applets = [
+              "panel1:left:0:menu@cinnamon.org:0"
+              "panel1:left:1:separator@cinnamon.org:1"
+              "panel1:left:2:grouped-window-list@cinnamon.org:2"
+              "panel1:right:0:systray@cinnamon.org:3"
+              "panel1:right:1:xapp-status@cinnamon.org:4"
+              "panel1:right:2:notifications@cinnamon.org:5"
+              "panel1:right:3:printers@cinnamon.org:6"
+              "panel1:right:4:removable-drives@cinnamon.org:7"
+              "panel1:right:5:keyboard@cinnamon.org:8"
+              "panel1:right:6:favorites@cinnamon.org:9"
+              "panel1:right:7:network@cinnamon.org:10"
+              "panel1:right:8:sound@cinnamon.org:11"
+              "panel1:right:9:power@cinnamon.org:12"
+              "panel1:right:10:calendar@cinnamon.org:13"
+              "panel1:right:11:cornerbar@cinnamon.org:14"
+            ];
+            favorite-apps = [
+              "firefox.desktop"
+              "cinnamon-settings.desktop"
+              "org.gnome.Terminal.desktop"
+              "nemo.desktop"
+            ];
+            next-applet-id = mkInt32 15;
+            panel-edit-mode = false;
+            panel-zone-icon-sizes = "[{\"panelId\":1,\"left\":0,\"center\":0,\"right\":24}]";
+            panel-zone-symbolic-icon-sizes = "[{\"panelId\": 1, \"left\": 28, \"center\": 28, \"right\": 16}]";
+            panel-zone-text-sizes = "[{\"panelId\":1,\"left\":0,\"center\":0,\"right\":0}]";
+            panels-autohide = [
+              "1:false"
+              "2:false"
+            ];
+            panels-enabled = [ "1:0:bottom" ];
+            panels-height = [
+              "1:48"
+              "2:40"
+            ];
+            panels-hide-delay = [
+              "1:0"
+              "2:0"
+            ];
+            panels-show-delay = [
+              "1:0"
+              "2:0"
+            ];
+          };
+
+          "org/cinnamon/cinnamon-session" = {
+            quit-time-delay = mkInt32 60;
+          };
+
+          "org/gnome/desktop/input-sources" = {
+            sources = [
+              (mkTuple [
+                "xkb"
+                "pl"
+              ])
+            ];
+          };
+
+          "org/cinnamon/desktop/interface" = {
+            clock-show-date = true;
+            clock-show-seconds = true;
+            cursor-size = mkInt32 24;
+            cursor-theme = "macOS";
+            font-name = "Ubuntu 10";
+            gtk-theme = "Mint-Y-Dark-Orange";
+            icon-theme = "Mint-Y-Yaru";
+            text-scaling-factor = 1.0;
+            toolkit-accessibility = false;
+          };
+
+          "org/cinnamon/desktop/notifications" = {
+            bottom-notifications = true;
+            display-notifications = true;
+            notification-duration = mkInt32 4;
+          };
+
+          "org/cinnamon/desktop/peripherals/keyboard" = {
+            numlock-state = true;
+          };
+
+          "org/cinnamon/desktop/peripherals/mouse" = {
+            accel-profile = "flat";
+            double-click = mkInt32 540;
+            drag-threshold = mkInt32 8;
+            speed = 0.0;
+          };
+
+          "org/cinnamon/desktop/peripherals/touchpad" = {
+            speed = 0.0;
+          };
+
+          "org/cinnamon/desktop/screensaver" = {
+            layout-group = mkInt32 0;
+          };
+
+          "org/cinnamon/desktop/sound" = {
+            event-sounds = false;
+          };
+
+          "org/cinnamon/gestures" = {
+            enabled = false;
+            pinch-percent-threshold = mkUint32 40;
+            swipe-down-2 = "PUSH_TILE_DOWN::end";
+            swipe-down-3 = "TOGGLE_OVERVIEW::end";
+            swipe-down-4 = "VOLUME_DOWN::end";
+            swipe-left-2 = "PUSH_TILE_LEFT::end";
+            swipe-left-3 = "WORKSPACE_NEXT::end";
+            swipe-left-4 = "WINDOW_WORKSPACE_PREVIOUS::end";
+            swipe-percent-threshold = mkUint32 60;
+            swipe-right-2 = "PUSH_TILE_RIGHT::end";
+            swipe-right-3 = "WORKSPACE_PREVIOUS::end";
+            swipe-right-4 = "WINDOW_WORKSPACE_NEXT::end";
+            swipe-up-2 = "PUSH_TILE_UP::end";
+            swipe-up-3 = "TOGGLE_EXPO::end";
+            swipe-up-4 = "VOLUME_UP::end";
+            tap-3 = "MEDIA_PLAY_PAUSE::end";
+          };
+
+          "org/cinnamon/launcher" = {
+            check-frequency = mkInt32 300;
+            memory-limit = mkInt32 2048;
+          };
+
+          "org/cinnamon/muffin" = {
+            tile-maximize = true;
+          };
+
+          "org/cinnamon/settings-daemon/peripherals/keyboard" = {
+            numlock-state = "on";
+          };
+
+          "org/cinnamon/settings-daemon/plugins/color" = {
+            night-light-last-coordinates = mkTuple [
+              52.25
+              21.0
+            ];
+          };
+
+          "org/cinnamon/settings-daemon/plugins/power" = {
+            lid-close-ac-action = "suspend";
+            lid-close-battery-action = "suspend";
+            lid-close-suspend-with-external-monitor = false;
+            sleep-display-ac = mkInt32 600;
+            sleep-display-battery = mkInt32 600;
+            sleep-inactive-ac-timeout = mkInt32 0;
+            sleep-inactive-battery-timeout = mkInt32 0;
+          };
+
+          "org/cinnamon/settings-daemon/plugins/xsettings" = {
+            antialiasing = "rgba";
+            buttons-have-icons = false;
+            hinting = "slight";
+          };
+
+          "org/cinnamon/theme" = {
+            name = "Mint-Y-Dark-Orange";
+          };
+        };
+      }
+    ];
+  };
 }
