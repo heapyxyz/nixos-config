81431e032edd9e0fc41bb409adce4eea334ad4b1
heapy <63428380+heapyxyz@users.noreply.github.com>
2026-05-03 16:14:51 +0200

    feat!: switch from cinnamon to gnome

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,9 +1,13 @@
-{ ... }:
-
+{ config, flake-dir, ... }:
+let
+  link = f: config.lib.file.mkOutOfStoreSymlink "${flake-dir}/modules/home/${f}";
+in
 {
   imports = [
     ./config
   ];
+
+  fonts.fontconfig.enable = true;
 
   home = {
     username = "heapy";
@@ -14,6 +18,28 @@
     # make ghostty default terminal
     sessionVariables = {
       TERMINAL = "ghostty";
+    };
+
+    file = {
+      ".local/share/backgrounds" = {
+        source = link "backgrounds";
+        force = true;
+      };
+
+      # https://github.com/vinceliuice/MacTahoe-icon-theme
+      # used icons from the `main` branch because latest release isn't up-to-date
+      ".local/share/icons/MacTahoe" = {
+        source = link "icons/MacTahoe";
+        force = true;
+      };
+      ".local/share/icons/MacTahoe-dark" = {
+        source = link "icons/MacTahoe-dark";
+        force = true;
+      };
+      ".local/share/icons/MacTahoe-dark-bold" = {
+        source = link "icons/MacTahoe-dark-bold";
+        force = true;
+      };
     };
   };
 }
