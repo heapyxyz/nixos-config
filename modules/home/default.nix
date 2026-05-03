56d784841d4c0a096b0314ded81827efee97d9f8
heapy <63428380+heapyxyz@users.noreply.github.com>
2026-05-03 16:10:00 +0200

    feat(gnome): macos icons and wallpaper

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -7,6 +7,8 @@ in
     ./config
   ];
 
+  fonts.fontconfig.enable = true;
+
   home = {
     username = "heapy";
     homeDirectory = "/home/heapy";
@@ -18,6 +20,26 @@ in
       TERMINAL = "ghostty";
     };
 
-    file.".local/share/backgrounds/background.jpg".source = link "background.jpeg";
+    file = {
+      ".local/share/backgrounds" = {
+        source = link "backgrounds";
+        force = true;
+      };
+
+      # https://github.com/vinceliuice/MacTahoe-icon-theme
+      # used icons from the `main` branch because latest release isn't up-to-date
+      ".local/share/icons/MacTahoe" = {
+        source = link "icons/MacTahoe";
+        force = true;
+      };
+      ".local/share/icons/MacTahoe-dark" = {
+        source = link "icons/MacTahoe-dark";
+        force = true;
+      };
+      ".local/share/icons/MacTahoe-dark-bold" = {
+        source = link "icons/MacTahoe-dark-bold";
+        force = true;
+      };
+    };
   };
 }
--- utils/gnome-dump.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# app-picker-layout causes dconf2nix to fail, so just delete it
-nix-shell -p dconf2nix --run "
-    dconf dump /org/gnome/ > gnome_dump.txt && \
-    sed -i '/app-picker-layout/d' gnome_dump.txt && \
-    dconf2nix -i gnome_dump.txt -o gnome_dump.nix
-"
-rm gnome_dump.txt
