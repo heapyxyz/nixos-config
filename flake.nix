0792735b777a45c67fc2f0e2f5c7d7bb036bac8c
heapy <63428380+heapyxyz@users.noreply.github.com>
2026-07-19 20:12:37 +0200

    chore: update flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager";
