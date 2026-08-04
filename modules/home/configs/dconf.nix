c5c40fb8e86f821b0cb9f4e8c295d64f156311f2
heapyxyz <63428380+heapyxyz@users.noreply.github.com>
2026-08-04 22:36:34 +0200

    chore: replace vscode with zed in gnome dock

--- modules/home/configs/dconf.nix
+++ modules/home/configs/dconf.nix
@@ -144,7 +144,7 @@ with lib.gvariant;
           "brave-browser.desktop"
           "steam.desktop"
           "equibop.desktop"
-          "code.desktop"
+          "dev.zed.Zed.desktop"
           "org.gnome.Nautilus.desktop"
           "com.mitchellh.ghostty.desktop"
           "termius-app.desktop"
