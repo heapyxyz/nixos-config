c631012c4310f13e52678ac91e7e5ecf77d38677
heapyxyz <63428380+heapyxyz@users.noreply.github.com>
2026-08-24 19:48:29 +0200

    feat(zsh): `rebuild-boot` alias

--- modules/system/shared/packages.nix
+++ modules/system/shared/packages.nix
@@ -29,6 +29,7 @@
     shellAliases = {
       ff = "fastfetch";
       rebuild = "sudo nixos-rebuild switch --flake ${flake-dir}#${hostname}";
+      rebuild-boot = "sudo nixos-rebuild boot --flake ${flake-dir}#${hostname}";
       cleanup = "sudo nix-collect-garbage -d; nix-store --optimise";
       update = "cd ${flake-dir}; nix flake update; cd -";
       shell = "nix-shell --command zsh ";
