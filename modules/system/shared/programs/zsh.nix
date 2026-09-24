ccf61e54587c0d57e9631696e3977e9ff2eeab0c
heapyxyz <63428380+heapyxyz@users.noreply.github.com>
2026-09-24 14:31:38 +0200

    chore(zsh): separate `cleanup` alias into 2 aliases

--- modules/system/shared/programs/zsh.nix
+++ modules/system/shared/programs/zsh.nix
@@ -11,7 +11,8 @@
       ff = "fastfetch";
       rebuild = "sudo nixos-rebuild switch --flake ${flake-dir}#${hostname}";
       rebuild-boot = "sudo nixos-rebuild boot --flake ${flake-dir}#${hostname}";
-      cleanup = "sudo nix-collect-garbage -d; nix-store --optimise";
+      cleanup = "sudo nix-collect-garbage -d";
+      optimize = "nix-store --optimise";
       update = "cd ${flake-dir}; nix flake update; cd -";
       shell = "nix-shell --command zsh ";
       ls = "eza ";
